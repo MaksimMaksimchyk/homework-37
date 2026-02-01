479b2cbf85891a04ecf0d9fb2a9fd8eb2cd0360e
MaksimMaksimchyk <maksim.maksimchyk@yandex.by>
2026-02-01 20:24:06 +0300

    Add UI

--- app/src/main/java/com/example/homework_37/MainActivity.kt
+++ app/src/main/java/com/example/homework_37/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.example.homework_37.ui.theme.Homework37Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Homework37Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainUi(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +40,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Homework37Theme {
-        Greeting("Android")
+fun MainUi(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AsyncImage(
+            model = "https://i.pravatar.cc/",
+            contentDescription = "Avatar",
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            "Random User",
+            modifier = Modifier.padding(top = 22.dp),
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Text(
+            text = "Phone number: + 123 456 789",
+            modifier = Modifier
+                .padding(top = 22.dp)
+                .align(Alignment.Start),
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Text(
+            text = "City: Moscow",
+            modifier = Modifier
+                .padding(top = 12.dp)
+                .align(Alignment.Start),
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = {}, modifier = Modifier.padding(bottom = 12.dp)
+        ) {
+            Text(
+                text = "Download Avatar Image",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
     }
 }
